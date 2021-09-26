b7c9d6b2e7b810faaf935219dacbf46f6d219c23
1023150697@qq.com <1023150697@qq.com>
2021-09-26 14:50:16 +0800

    update

---
+++ src/base/BaseConsole.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace eazy\base;
+
+use Symfony\Component\Console\Command\Command;
+
+class BaseConsole extends Command
+{
+
+}
---
+++ src/console/Console.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace eazy\console;
+
+use eazy\base\BaseConsole;
+
+class Console extends BaseConsole
+{
+
+}
